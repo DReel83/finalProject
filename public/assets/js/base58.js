412fa04243e571d63d144e52237a2a3efb58f6fa
David Reel <davidpreel@gmail.com>
2016-11-29 19:40:31 -0500

    started working on creating the base for the url

---
+++ public/assets/js/base58.js
@@ -0,0 +1,6 @@
+//Here the alphabet that will be used for the shortened URL is defined. 
+// I left out 0,O,I to avoid confusion aon the users end.
+var alphabet = "123456789abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ";
+
+// Here to length of the alphabet is used for the base.
+var base = alphabet.length;
