6232429192cbabc12e7db4fccd3ec976d391837e
David Reel <davidpreel@gmail.com>
2016-12-01 19:27:08 -0500

    Finished decode function and exported encode/decode functions for use in app.js

--- public/assets/js/base58.js
+++ public/assets/js/base58.js
@@ -18,6 +18,20 @@ function encode(num){
   return encoded;	
 }
 
-//The decoding function converts the short URL into it's base10 
+//The decoding function converts the short URL into it's base10 integer 
 //equivalent so the long URL can be retrieved from th DB.
 
+function decode(str) {
+	var decoded = 0;
+	while (str){
+		var index = alphabet.indexOf(str[0]);
+	    var power = str.length -1;
+	    decoded += index * (Math.pow(base, power));
+	    str = str.substring(1);
+	}
+	return decoded;
+}
+
+// Eport functionn for use in app.js
+module.exports.encode = encode;
+module.exports.decode = decode;
