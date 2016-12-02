e715414225d29d7611f01648e2cb1e4c020ee1e5
David Reel <davidpreel@gmail.com>
2016-12-01 19:10:32 -0500

    added encode function to base58.js

--- public/assets/js/base58.js
+++ public/assets/js/base58.js
@@ -4,3 +4,18 @@ var alphabet = "123456789abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ";
 
 // Here to length of the alphabet is used for the base.
 var base = alphabet.length;
+
+//The encoding function below will convert the database unique ID (base10) into
+//the base58 string that will create a shorter URL.
+
+function encode(num){
+	var encoded = "";
+	while (num){
+		var remainder = num % base;
+		num = Math.floor(num / base);
+		encoded = alphabet[remainder].toString() + encoded;
+	}
+  return encoded;	
+}
+
+// 
