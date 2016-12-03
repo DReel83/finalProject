f231ecaefd0b10613383dc7c0edd83dcdec95cb9
David Reel <davidpreel@gmail.com>
2016-12-03 11:44:05 -0500

    added config.js for host and db connection info

---
+++ public/assets/js/config.js
@@ -0,0 +1,16 @@
+// The follow will be responsible for storing connection infomation
+//as well as the shortener host.
+
+var config = {};
+
+config.db = {};
+//
+ config.webhost = 'http://localhost:3000/';
+
+ //MongoDB host and DB name
+ config.db.host = 'localhost';
+
+ config.db.name = 'url_shortener';
+
+// Export to make available
+ module.exports = config;
